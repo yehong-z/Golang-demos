2b48444340bd6b730f63fab06e747ac6683fe240
yehong <1059463820@qq.com>
2023-05-20 14:11:15 +0800

    logrus基本使用

---
+++ logrus-demo/logger/log.go
@@ -0,0 +1,30 @@
+package logger
+
+import (
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+var logger *logrus.Logger
+
+func init() {
+	// 创建新的 logger
+	logger = logrus.New()
+	// 设置日志格式为 JSON 格式
+	//logger.SetFormatter(&logrus.JSONFormatter{})
+	// 记录时间戳
+	logger.SetReportCaller(true)
+	// 设置日志级别
+	logger.SetLevel(logrus.DebugLevel)
+	// 输出到标准输出流
+	logger.SetOutput(os.Stdout)
+	// 对每个 logger 的日志添加请求信息、错误类型、错误代码、堆栈追踪等字段
+	//logger.Hooks.Add(&ContextHook{})
+}
+func InitLog() {
+
+}
+
+func Info(v ...interface{}) {
+	logger.Info(v...)
+}
---
+++ logrus-demo/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/sirupsen/logrus"
+
+func init() {
+
+}
+
+func main() {
+	logrus.Info("salkdfj")
+
+}
