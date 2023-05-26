5022346ff37c9c4dc5c65b6b6e059efaabb99d0a
yehong <1059463820@qq.com>
2023-05-26 12:26:41 +0800

    config toml

---
+++ config-demo/config_test.go
@@ -0,0 +1,11 @@
+package config_demo
+
+import "testing"
+
+func TestConfig(t *testing.T) {
+	PrintConfig()
+}
+
+func TestConfig2(t *testing.T) {
+	PrintTomlFromFile()
+}
---
+++ config-demo/toml.go
@@ -0,0 +1,58 @@
+package config_demo
+
+import (
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+	"io/ioutil"
+	"path"
+)
+
+type Config struct {
+	Title  string
+	Server struct {
+		Host    string
+		Port    int
+		Timeout int
+	}
+	Database struct {
+		Name     string
+		Username string
+		Password string
+		Port     int
+	}
+}
+
+func fromTomlFile(c Config, p string) error {
+	content, err := ioutil.ReadFile(path.Clean(p))
+	if err != nil {
+		return err
+	}
+
+	dec := unicode.BOMOverride(transform.Nop)
+	content, _, err = transform.Bytes(dec, content)
+	if err != nil {
+		return err
+	}
+
+	_, err = toml.Decode(string(content), c)
+	return err
+}
+
+func PrintTomlFromFile() {
+	var conf Config
+	err := fromTomlFile(conf, "./test.conf")
+	if err != nil {
+		return
+	}
+	fmt.Printf("%+v\n", conf)
+}
+
+func PrintConfig() {
+	var conf Config
+	if _, err := toml.DecodeFile("test.conf", &conf); err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", conf)
+}
